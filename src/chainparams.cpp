21eac603af84995de101e65cd719853c37fdec27
diagraphic <diagraphic@yahoo.com>
2021-12-12 18:33:48 -0500

    qt changes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -98,7 +98,7 @@ public:
          * The message start string is designed to be unlikely to occur in normal data.
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
-         */
+         */ 
         pchMessageStart[0] = 0xf6;
         pchMessageStart[1] = 0xbe;
         pchMessageStart[2] = 0xb3;
