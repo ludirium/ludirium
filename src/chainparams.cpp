ad4c15f67940eb0200d91f8dacb2a5b6493bcefe
diagraphic <diagraphic@yahoo.com>
2021-12-13 20:52:04 -0500

    test net fix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -225,7 +225,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
-        bech32_hrp = "tb";
+        bech32_hrp = "tlu";
 
         vFixedSeeds = std::vector<uint8_t>(std::begin(chainparams_seed_test), std::end(chainparams_seed_test));
 
@@ -355,7 +355,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
 
-        bech32_hrp = "tl";
+        bech32_hrp = "tlu";
 
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
